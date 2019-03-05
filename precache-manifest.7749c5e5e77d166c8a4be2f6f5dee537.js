11f8c14400f866bffb3c4bba49bda1f177eabd6c
therisaz <therisaz@pm.me>
2019-03-05 23:47:33 +0700

    Updates

---
+++ precache-manifest.7749c5e5e77d166c8a4be2f6f5dee537.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "485cb9a1fd5467055c8a",
+    "url": "/portfolio/static/css/main.79b83aed.chunk.css"
+  },
+  {
+    "revision": "485cb9a1fd5467055c8a",
+    "url": "/portfolio/static/js/main.485cb9a1.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/portfolio/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d068d26e2b1925e2078025a409126934",
+    "url": "/portfolio/static/media/badge.d068d26e.png"
+  },
+  {
+    "revision": "bd9805cba4fdbd10e6f9d097dab9225f",
+    "url": "/portfolio/static/media/chalkduster.bd9805cb.ttf"
+  },
+  {
+    "revision": "004679d57676594d935646c18c32be19",
+    "url": "/portfolio/static/media/quote.004679d5.svg"
+  },
+  {
+    "revision": "a0c5074acbd2970110cafd594fca804a",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.de606ce14c34e3c301b15abc1ba2ef3a.js"
+  "/portfolio/precache-manifest.7749c5e5e77d166c8a4be2f6f5dee537.js"
 );
 
 workbox.clientsClaim();
